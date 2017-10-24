3c461da08658063ca20e542804452aee62831e41
shaoming Huang <msn8971@gmail.com>
2017-10-24 15:00:20 +0800

    View_Index

--- FoundCity_V2/FoundCity_V2/Controllers/MemberController.cs
+++ FoundCity_V2/FoundCity_V2/Controllers/MemberController.cs
@@ -8,10 +8,32 @@ namespace FoundCity_V2.Controllers
 {
     public class MemberController : Controller
     {
+        #region 會員中心首頁
         // GET: Member
         public ActionResult Index()
         {
             return View();
         }
+        #endregion
+        #region 基本資料
+        public ActionResult Basic() {
+            return View();
+        }
+        #endregion
+        #region 變更密碼
+        public ActionResult Modify() {
+            return View();
+        }
+        #endregion
+        #region 會員登入
+        public ActionResult Login() {
+            return View();
+        }
+        #endregion
+        #region 會員註冊
+        public ActionResult Register() {
+            return View();
+        }
+        #endregion
     }
 }
