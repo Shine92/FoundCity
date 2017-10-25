3cfefb5963e9865bc589107235bb801d6d561f3d
Shine92 <msn8971@gmail.com>
2017-10-25 17:01:51 +0800

    1025Simon
    1025

--- FoundCity_V2/FoundCity_V2/Controllers/MemberController.cs
+++ FoundCity_V2/FoundCity_V2/Controllers/MemberController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using FoundCity_V2.Models;
 namespace FoundCity_V2.Controllers
 {
     public class MemberController : Controller
     {
+        Member MemberData = new Member();
+
         #region 會員中心首頁
         // GET: Member
         public ActionResult Index()
@@ -32,6 +34,11 @@ namespace FoundCity_V2.Controllers
         #endregion
         #region 會員註冊
         public ActionResult Register() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Member newMember) {
             return View();
         }
         #endregion
